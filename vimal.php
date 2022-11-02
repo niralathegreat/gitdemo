3e27536c24f4adbaf1dee123165b0b031b96c245
Surya Kanth <skant@bamko.net>
2022-11-02 12:33:31 +0530

    add new feature

--- vimal.php
+++ vimal.php
@@ -1,3 +1,4 @@
 <?php echo "Hi.. vimal this side. How are you."; ?>
 <?php echo "for push hint in local branch required git push --set-upstream origin vimal command "; ?>
+<?php echo "add new feture"; ?>
 
